f34212cdcb8b02402c8aa87d7410edd36d5e0f10
Gonzalo Porras Lopez <gonzalo@MacBook-Air-de-Gonzalo.local>
2024-07-21 13:59:39 -0500

    probando interface despliegue

--- app/Http/Interfaces/IReclamacion.php
+++ app/Http/Interfaces/IReclamacion.php
@@ -5,6 +5,10 @@ namespace App\Http\Interfaces;
 use App\Http\interfaces\ICRUD;
 use App\Models\Reclamacion;
 
-interface IReclamacion extends ICRUD{
+interface IReclamacion{
     public function show(String $id):Reclamacion;
+    public function index();
+    public function store(Object $object);
+    public function update(Object $object, string $id);
+    public function destroy(string $id);
 }
